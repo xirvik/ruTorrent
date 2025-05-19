4e3c5b3af45dde3ce6e03f4fca35d939dedce6c6
Toha <tohenk@yahoo.com>
2025-05-20 00:08:16 +0700

    Improve diskspace usage tooltip. (#2930)
    Signed-off-by: Toha <tohenk@yahoo.com>

--- plugins/diskspace/lang/pt-br.js
+++ plugins/diskspace/lang/pt-br.js
@@ -1,11 +1,12 @@
 ﻿/*
  * PLUGIN DISKSPACE
  *
- * Portuguese (Brazil) language file.
+ * Brazilian Portuguese language file.
  *
  * Author: 
  */
 
- theUILang.diskNotification	= "Atenção! O disco está cheio. O rTorrent pode não funcionar corretamente, e nenhum dado será baixado até que você libere espaço em disco.";
+ theUILang.diskNotification = "Atenção! O disco está cheio. O rTorrent pode não funcionar corretamente, e nenhum dado será baixado até que você libere espaço em disco.";
+ theUILang.diskUsage = "%USED%/%TOTAL% (%FREE% free)";
 
 thePlugins.get("diskspace").langLoaded();
